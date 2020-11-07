1d84f75e003aec344f79b054da254319535db5ce
Ivanio Martins <ivaniomartins1991@gmail.com>
2020-11-07 15:40:01 -0300

    Project created

---
+++ src/main/java/com/martins/workshopmongodb/ProjectbudgetSpringMongoApplication.java
@@ -0,0 +1,13 @@
+package com.martins.workshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectbudgetSpringMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectbudgetSpringMongoApplication.class, args);
+	}
+
+}
